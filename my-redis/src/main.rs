dfdf3d4ac7f34b6406a5139b5d32dca8fe31b536
Mohammed Essehemy <mohammedessehemy@gmail.com>
2022-01-23 17:29:49 +0400

    feat: share state

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,5 +1,11 @@
-use mini_redis::{Connection, Frame};
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
 use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+use mini_redis::Command::{self, Get, Set};
+use bytes::Bytes;
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
@@ -8,22 +14,20 @@ async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind(format!("{host}:{port}")).await.unwrap();
 
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // The second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
-        // process(socket).await;
+        // Clone the handle to the hash map.
+        let db = db.clone();
+        println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
-async fn process(socket: TcpStream) {
-    use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // A hashmap is used to store data
-    let mut db = HashMap::new();
-
+async fn process(socket: TcpStream, db: Db) {
     // Connection, provided by `mini-redis`, handles parsing frames from
     // the socket
     let mut connection = Connection::new(socket);
@@ -32,11 +36,13 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
+                let mut db = db.lock().unwrap();
                 // The value is stored as `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     // `Frame::Bulk` expects data to be of type `Bytes`. This
                     // type will be covered later in the tutorial. For now,
