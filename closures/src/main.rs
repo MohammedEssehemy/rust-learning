f16e376879314be091fa56dfde095fd95613dc25
Mohammed Essehemy <mohammedessehemy@gmail.com>
2022-05-15 14:20:55 +0400

    refactor: closures

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -24,63 +24,71 @@
 // assert_eq!(v2, vec![2, 3, 4]);
 // }
 
-// #[derive(PartialEq, Debug)]
-// struct Shoe {
-//     size: u32,
-//     style: String,
-// }
-
-// fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
-//     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn filters_by_size() {
-//         let shoes = vec![
-//             Shoe {
-//                 size: 10,
-//                 style: String::from("sneaker"),
-//             },
-//             Shoe {
-//                 size: 13,
-//                 style: String::from("sandal"),
-//             },
-//             Shoe {
-//                 size: 10,
-//                 style: String::from("boot"),
-//             },
-//         ];
-
-//         let in_my_size = shoes_in_my_size(shoes, 10);
-
-//         assert_eq!(
-//             in_my_size,
-//             vec![
-//                 Shoe {
-//                     size: 10,
-//                     style: String::from("sneaker")
-//                 },
-//                 Shoe {
-//                     size: 10,
-//                     style: String::from("boot")
-//                 },
-//             ]
-//         );
-//     }
-// }
-
-// fn main() {}
-
-
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
 }
-use crate::List::{Cons, Nil};
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let shoes = vec![
+    Shoe {size: 13, style: "test".to_owned()}, 
+    Shoe {size: 15, style: "test3".to_owned()}
+    ];
+    println!("All shoes: {:?}", shoes);
+    let my_size = 15;
+    println!("My size is {}, Which ones are on my size? {:?}", my_size, shoes_in_my_size(shoes, my_size));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_my_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+}
+
+
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+// use crate::List::{Cons, Nil};
+// fn main() {
+//     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+// }
